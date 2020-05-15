ad422197c691d471e90bf7e4478654dfa6293d6d
Naman00 <42418828+Naman00@users.noreply.github.com>
2020-05-15 14:06:08 +0530

    Add files via upload

---
+++ binary.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define ral(i,n) for(int i=1;i<=n;i++)
+#define mod 1000000007
+#define vect vector<int>
+#define pb push_back
+#define lli long long int <iostream>
+using namespace std;
+// int binarysearch(int arr[],int low,int high,int key)
+// {
+// 	while(low<=high)
+// 	{
+// 	    int mid = (low+high)/2;
+// 		if(arr[mid]==key)
+// 			return mid;
+// 		else if(arr[mid] > key)
+// 			high = mid-1;
+// 		else if(arr[mid] < key)
+// 			low = mid+1;
+
+// 	}
+// 	return -1;
+// }
+// int main()
+// {
+// // 	int arr[]={1,2,3,4,5,6,8};
+// // 	int a = binarysearch(arr,0,7,7);
+// // 	if(a!=-1)
+// // 		cout<<"element found at index:"<<a;
+// // 	else
+// // 		cout<<"element not found";
+
+//     #ifndef ONLINE_JUDGE
+// 		freopen("input.txt","r",stdin);
+// 		freopen("output.txt","w",stdout);
+// 	#endif
+//     int arr[10];
+// 	for(int i=0;i<10;i++)
+// 	{
+// 		cin>>arr[i];
+// 	}
+//     int n;
+//     cin>>n;
+// 	int a = binarysearch(arr,0,9,n);
+// 	if(a!=-1)
+// 		cout<<"element found at index:"<<a;
+// 	else
+// 		cout<<"element not found";
+// }
+int main()
+{
+	int a;
+	cin>>a;
+	cout<<a+10;
+}
